e1146d522f610aec4d8b0883cfcef2636b49e553
Hasenpfote <Hasenpfote@gmail.com>
2018-09-17 10:55:43 +0900

    Bump version to 0.5.0

--- image_packer/version.py
+++ image_packer/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 #  -*- coding: utf-8 -*-
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 VERSION = __version__.split('.')
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.7',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 4 - Beta',
             'Environment :: Other Environment',
             'Intended Audience :: Developers',
             'Topic :: Software Development :: Libraries :: Python Modules',
